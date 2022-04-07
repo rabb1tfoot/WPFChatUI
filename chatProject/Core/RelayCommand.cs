3c83681158b34ffe7220f023318fd87d4a7b2953
rabb1tfoot <46916924+rabb1tfoot@users.noreply.github.com>
2022-04-07 10:39:57 +0900

    이전 백업

---
+++ chatProject/Core/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace chatProject.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<Object> execute;
+        private Func<Object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<Object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
---
+++ chatProject/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using chatProject.Core;
+using chatProject.MVVM.Model;
+
+namespace chatProject.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        // command //
+        public RelayCommand SendCommand { get; set; }
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact {
+            get { return _selectedContact; }
+            set { _selectedContact = value;
+                OnPropertyChanged();
+            }           
+        }
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    firstMess = false
+                });
+                Message = "";
+            });
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMVvLXd.png",
+                Message = "Test",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                firstMess = false
+            });
+
+            for(int i = 0; i < 3; ++i)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Allison",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMVvLXd.png",
+                Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    firstMess = false
+                });
+            }
+
+            for (int i = 0; i < 4; ++i)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bunny",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMVvLXd.png",
+                Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true,
+                });
+            }
+            Messages.Add(new MessageModel
+            {
+                Username = "Bunny",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMVvLXd.png",
+                Message = "Test",
+                Time = DateTime.Now,
+                IsNativeOrigin = true,
+            });
+            for(int i = 0; i < 5; ++i)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Allision {i}",
+                    ImageSource = "https://i.imgur.com/yMVvLXd.png",
+                    Messages = Messages
+                });
+            }
+        }
+    }
+}
